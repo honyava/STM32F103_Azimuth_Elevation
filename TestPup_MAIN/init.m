246f6a5be8a966b2f15f043920f58fac8ff7ab94
honyava <88489271+honyava@users.noreply.github.com>
2023-04-18 08:33:16 +0300

    1
    1

---
+++ TestPup_MAIN/init.m
@@ -0,0 +1,93 @@
+disp('Start Init');
+
+dev_ports = struct(...
+    'pup', find_pup_port(), ...
+    'pmes', find_pmes_port(), ...
+    'kama', find_kama_port() ...
+);
+
+% dev_ports = struct(...
+%     'pup', find_pup_port(), ...
+%     'pmes', [], ...
+%     'kama', find_kama_port() ...
+% );
+
+% dev_ports = struct(...
+%     'pup', find_pup_port(), ...
+%     'pmes', [], ...
+%     'kama', [] ...
+% );
+
+disp('Complete Init');
+
+%% function
+function pup_port = find_pup_port()
+    disp("Поиск ПУП...");
+    baudrate = 115200;
+    ports = serialportlist("available");
+
+    for port = ports
+        fprintf("Попытка подключения к порту %s\n", port);
+        pup_port = serialport(port, baudrate, 'Timeout', 0.5, 'StopBits',2);
+        pup_port.flush();
+
+        w = warning('off', 'all');
+        data = pup_port.read(6, "uint8");
+        warning(w);
+
+        if ~isempty(data)
+            return;
+        end
+
+    end
+
+    disp('Плата ПУП не найдена');
+    pup_port = [];
+
+end
+
+function pmes_port = find_pmes_port()
+    disp("Поиск платы измерений...");
+    baudrate = 115200;
+    ports = serialportlist("available");
+
+    for port = ports
+        fprintf("Попытка подключения к порту %s\n", port);
+        pmes_port = serialport(port, baudrate, Timeout = 5);
+        pmes_port.flush();
+
+        w = warning('off', 'all');
+        pmes_port.write("TEST", "uint8");
+        data = pmes_port.read(8, "single");
+        warning(w);
+
+        if ~isempty(data)
+            return;
+        end
+    end
+    disp('Плата измерений не найдена');
+    pmes_port = [];
+end
+
+function dev_port = find_kama_port()  
+    disp("Поиск порта для Камы..."); 
+    baudrate = 115200;
+    ports = serialportlist("available");    
+    ports = "COM10";
+    for port = ports
+        fprintf("Попытка подключения к порту %s\n", port);
+        dev_port = serialport(port, baudrate, Timeout = 5);
+        dev_port.flush();
+
+        w = warning('off', 'all');
+        data = 5;
+        warning(w);
+
+        if ~isempty(data)
+            return;
+        end
+    end
+    disp("Порта для Камы не найден..."); 
+    dev_port = [];
+end
+
